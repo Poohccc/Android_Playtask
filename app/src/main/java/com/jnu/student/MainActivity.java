e5ee3e7a46438b922fff7d0002fa8745ff243966
ccc <1535758024@qq.com>
2023-10-17 09:45:06 +0800

    第三次

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -1,12 +1,15 @@
 package com.jnu.student;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button button =findViewById(R.id.click_me_button);
         button.setOnClickListener(new Click());
+
 
     }
 
@@ -31,6 +35,24 @@ public class MainActivity extends AppCompatActivity {
             String temp=view1.getText().toString();
             view1.setText(view2.getText().toString());
             view2.setText(temp);
+            exchangeSuccessToast();
         }
+    }
+
+    private void exchangeSuccessToast() {
+        //Toast.makeText(this, "交换成功", Toast.LENGTH_SHORT).show();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("交换成功")
+                .setMessage("您已成功完成交换。")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
