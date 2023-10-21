a38bd79b1ff1b09e60966d54164606092bfdaeec
ccc <1535758024@qq.com>
2023-10-21 15:28:04 +0800

    Menu

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -4,16 +4,21 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,7 +27,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<BookItem> bookItems = new ArrayList<>();
+    private BookItemsAdapter bookItemsAdapter;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +39,15 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recycle_view_books = findViewById(R.id.recycle_view_books);
         //为线性布局
         recycle_view_books.setLayoutManager(new LinearLayoutManager(this));
-        ArrayList<BookItem> bookItems = new ArrayList<>();
         bookItems.add(new BookItem("软件项目管理案例教程（第4版）", R.drawable.book_2));
         bookItems.add(new BookItem("创新工程实践", R.drawable.book_no_name));
         bookItems.add(new BookItem("信息安全数学基础（第2版）", R.drawable.book_1));
 
-        BookItemsAdapter bookItemsAdapter = new BookItemsAdapter(bookItems);
+        bookItemsAdapter = new BookItemsAdapter(bookItems);
         recycle_view_books.setAdapter(bookItemsAdapter);
 
         registerForContextMenu(recycle_view_books);
-        ActivityResultLauncher<Intent> addItemLauncher;
+
         addItemLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
@@ -50,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                         Intent data = result.getData();
                         String name = data.getStringExtra("name");
                         bookItems.add(new BookItem(name, R.drawable.book_2));
-                        bookItemsAdapter.notifyItemRangeInserted(bookItems.size());
+                        bookItemsAdapter.notifyItemInserted(bookItems.size());
                     } else if (result.getResultCode() == Activity.RESULT_CANCELED) {
 
                     }
@@ -67,15 +72,24 @@ public class MainActivity extends AppCompatActivity {
         switch(item.getItemId()){
             case 0:
 
-
-
                 Intent intent =new Intent(MainActivity.this,BookItemDetailsActivity.class);
-
-                launcher.launch(intent);
+                addItemLauncher.launch(intent);
                 break;
+            case 1:
+                break;
+            case 2:
+                break;
+
             default:
                 return super.onContextItemSelected(item);
         }
+        return true;
     }
+
+
 }
+
+
+
+
 
